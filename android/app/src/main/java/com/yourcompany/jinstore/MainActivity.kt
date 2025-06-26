6cf01017ff9fabb059fc8853700e7691f92ee82d
nqhit <nguyenquochuy812003@gmail.com>
2025-06-26 14:52:05 +0700

    refactor: prebuild android app

--- android/app/src/main/java/com/yourcompany/jinstore/MainActivity.kt
+++ android/app/src/main/java/com/yourcompany/jinstore/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.yourcompany.firstapp
+package com.yourcompany.jinstore
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/yourcompany/jinstore/MainApplication.kt
+++ android/app/src/main/java/com/yourcompany/jinstore/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.yourcompany.firstapp
+package com.yourcompany.jinstore
 
 import android.app.Application
 import android.content.res.Configuration
