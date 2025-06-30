8ce1839c791e1b7abe0b4ac44a97fbc203d12cd9
nqhit <nguyenquochuy812003@gmail.com>
2025-06-30 09:22:14 +0700

    update url api

--- config/config.tsx
+++ config/config.tsx
@@ -1,1 +1,2 @@
-export const API_BASE_URL = 'http://192.168.88.224:8888/api';
+// export const API_BASE_URL = 'http://192.168.88.224:8888/api';
+export const API_BASE_URL = 'https://jinstore-api.onrender.com/api';
